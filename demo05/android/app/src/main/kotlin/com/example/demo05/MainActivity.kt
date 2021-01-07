6b2e8e1b0f8bc79b293465eb8a43fde4037cfdde
wuhao <2478731248@qq.com>
2021-01-07 17:19:30 +0800

    页面窗台保持

---
+++ demo05/android/app/src/main/kotlin/com/example/demo05/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo05/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import './keep_alive_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '保持页面状态',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: KeepAliveDemo(),
+    );
+  }
+}
+
+class KeepAliveDemo extends StatefulWidget {
+  @override
+  _KeepAliveDemoState createState() => _KeepAliveDemoState();
+}
+
+class _KeepAliveDemoState extends State<KeepAliveDemo>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('保持页面状态demo'),
+        bottom: TabBar(
+          controller: _controller,
+          tabs: [
+            Tab(icon: Icon(Icons.directions_car)),
+            Tab(icon: Icon(Icons.directions_transit)),
+            Tab(icon: Icon(Icons.directions_bike)),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          MyHomePage(),
+          MyHomePage(),
+          MyHomePage(),
+        ],
+      ),
+    );
+  }
+}
